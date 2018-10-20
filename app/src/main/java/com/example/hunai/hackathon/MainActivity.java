ffa4a739b76a7c2801e75a4be0393f871cc6a722
purihm <hunaid14@gmail.com>
2018-10-20 15:25:02 -0400

    Final Merge

--- app/src/main/java/com/example/hunai/hackathon/MainActivity.java
+++ app/src/main/java/com/example/hunai/hackathon/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, BubblePicker.class);
+                Intent intent=new Intent(MainActivity.this, HomeScreen.class);
                 startActivity(intent);
             }
         });
